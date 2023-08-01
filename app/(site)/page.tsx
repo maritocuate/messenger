5a11a8c358f4d2113b6eeca8bb4b4dd7354faa6c
Mario Quiroga <marioquiroga@MacBook-Air-de-Mario.local>
2023-08-01 20:30:43 -0300

    chore: index page setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import './styles.scss'
+
+export default function Home() {
+  return (
+    <div className="index-container sm:px-6 lg:px-8">
+      <div className='index-container--logo sm:mx-auto sm:w-full sm:max-w-md'>
+        <Image
+          alt='Logo'
+          height='48'
+          width='48'
+          src='/images/logo.png'
+        />
+        <h2 className='index-container--title'>Sign in</h2>
+      </div>
+    </div>
+  )
+}
