296b8ba55bf5d1b81495a97eae863095ff0c1063
Mario Quiroga <marioquiroga@MacBook-Air-de-Mario.local>
2023-08-02 20:55:26 -0300

    chore: update form component

--- app/(site)/components/AuthForm/index.tsx
+++ app/(site)/components/AuthForm/index.tsx
@@ -1,9 +1,12 @@
 'use client'
 import './styles.scss'
 import { useCallback, useState } from "react"
+import { BsGithub, BsGoogle } from 'react-icons/bs'
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 
 import Input from '@/app/components/inputs/Input'
+import Button from '@/app/components/Button'
+import AuthSocialButton from '../AuthSocialButton'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -48,15 +51,77 @@ const AuthForm = () => {
                     className="space-y-6"
                     onSubmit={handleSubmit(onSubmit)}
                 >
-                    <Input
+                    {variant === 'REGISTER' && (
+                        <Input
+                            disabled={loading}
+                            register={register}
+                            errors={errors}
+                            required
+                            id="name" 
+                            label="Name"
+                        />
+                    )}
+                    <Input 
                         disabled={loading}
                         register={register}
                         errors={errors}
                         required
-                        id="name" 
-                        label="Name"
+                        id="email" 
+                        label="Email address" 
+                        type="email"
                     />
+                    <Input 
+                        disabled={loading}
+                        register={register}
+                        errors={errors}
+                        required
+                        id="password" 
+                        label="Password" 
+                        type="password"
+                    />
+                    <div>
+                        <Button disabled={loading} fullWidth type="submit">
+                            {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+                        </Button>
+                    </div>
                 </form>
+
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="form-container--divisor">
+                            <div className="form-container--line" />
+                        </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton 
+                            icon={BsGithub} 
+                            onClick={() => socialAction('github')} 
+                        />
+                        <AuthSocialButton 
+                            icon={BsGoogle} 
+                            onClick={() => socialAction('google')} 
+                        />
+                    </div>
+                </div>
+
+                <div className="form-container--footer">
+                    <div>
+                        {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'} 
+                    </div>
+                    <div 
+                        onClick={toggleVariant} 
+                        className="underline cursor-pointer"
+                    >
+                        {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                    </div>
+                </div>
+
             </div>
         </div>
     )
