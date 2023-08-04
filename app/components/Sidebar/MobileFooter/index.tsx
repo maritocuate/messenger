912db2ed0654d9f4efee029ca87b56157fb0e486
maritocuate <mario300@gmail.com>
2023-08-04 01:58:04 -0300

    chore: add sidebar component

---
+++ app/components/Sidebar/MobileFooter/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import './styles.scss'
+
+const MobileFooter = () => {
+
+  return ( 
+    <div className="mobilefooter border-t-[1px] lg:hidden">
+      MobileFooter
+    </div>
+   );
+}
+ 
+export default MobileFooter
---
+++ app/components/Sidebar/index.tsx
@@ -0,0 +1,16 @@
+import MobileFooter from './MobileFooter'
+
+async function Sidebar({ children }: {
+    children: React.ReactNode,
+}) {
+  return (
+    <div className="h-full">
+        <MobileFooter />
+        <main className="lg:pl-20 h-full">
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default Sidebar
