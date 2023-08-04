1171a07214c4852f8f820fcf5b46c74c6b92a011
maritocuate <mario300@gmail.com>
2023-08-04 12:40:04 -0300

    chore: add mobile footer component

---
+++ app/components/Sidebar/MobileItem/index.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link"
+import clsx from "clsx"
+
+import './styles.scss'
+
+interface MobileItemProps {
+    href: string
+    icon: any
+    active?: boolean
+    onClick?: () => void
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({ 
+    href, 
+    icon: Icon, 
+    active,
+    onClick
+}) => {
+    const handleClick = () => {
+        if (onClick) {
+            return onClick()
+        }
+    }
+
+    return ( 
+        <Link 
+            onClick={handleClick} 
+            href={href} 
+            className={clsx(`mobileitem`, active && 'bg-gray-100 text-black',)}
+        >
+            <Icon className="h-6 w-6" />
+        </Link>
+    )
+}
+ 
+export default MobileItem
